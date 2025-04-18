493411f6f95e30c8d96b631cc92d91f0de4fd079
anuanaghavm <anaghavm2019@gmail.com>
2025-04-18 10:22:11 +0530

    update

--- AI/urls.py
+++ AI/urls.py
@@ -15,10 +15,40 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, re_path, include
+from rest_framework.authtoken.views import obtain_auth_token
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
+
+# Swagger schema view setup with public access
+schema_view = get_schema_view(
+    openapi.Info(
+        title="AI_TOOL BACKEND API",
+        default_version='v1',
+        description="API documentation for iMODA project",
+        terms_of_service="https://www.yourproject.com/terms/",
+        contact=openapi.Contact(email="contactimoda@gmail.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include("login.urls")),
     path("api/",include('questions.urls')),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
